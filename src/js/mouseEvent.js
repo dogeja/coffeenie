db6f34be415105289ede9e799b5d9882e0d9630a
dogeja <disk6473@gmail.com>
2023-03-08 04:06:59 +0900

    0308 reduce img size

--- src/js/mouseEvent.js
+++ src/js/mouseEvent.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   const mouseCursor = $("#mouseEvent");
   const moveCursor = (e) => {
     // 세로스크롤 접근시 가로스크롤 생성 방지
-    if (e.pageX < $(window).innerWidth() - 5) {
+    if (e.pageX < $(window).innerWidth() - 10) {
       mouseCursor.css({
         left: e.pageX,
         top: e.pageY,
